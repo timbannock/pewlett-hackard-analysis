8d5b4ccf70cb26720cda70d5ccd727ddd052571b
Bannock <Timothy.J.Bannock@disney.com>
2020-08-27 23:17:58 -0700

    Upload Deliverable 1 assets.

---
+++ queries/Employee_Database_challenge.sql
@@ -0,0 +1,30 @@
+-- Create the retirement_titles table and export to CSV
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows and export to CSV
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve the # employees by their most recent job title who are about to retire and export to CSV
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
