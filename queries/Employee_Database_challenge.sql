fc13b8ec92ddd38f6d5ce5086607b2703194b669
Bannock <Timothy.J.Bannock@disney.com>
2020-08-29 09:18:53 -0700

    Updated Challenge thru Deliverable 2.

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -28,3 +28,21 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
 
+-- Create a table of employees eligible for the mentorship program
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = ('9999-01-01'))
+AND (t.to_date = ('9999-01-01'))
+ORDER BY emp_no;
